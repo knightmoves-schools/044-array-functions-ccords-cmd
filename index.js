a544302646fad5711de74e53fb50223efa3929be
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 08:56:09 -0500

    Fix issues with automobiles array operations

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var automobiles = ["car","truck","motorcycle"];
-automobiles.push("airplane", "skateboard");
+var automobiles = ["car", "truck", "motorcycle"];
+automobiles.push(,"airplane", "skateboard");
 automobiles.unshift("bike,helicopter",);
 var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
-automobiles.pop(0);
+automobiles.pop();
 automobiles.shift();
 
 document.getElementById('result').innerHTML = automobiles;
+✕ should create a variable named automobiles that contains car, truck, motorcycle (257 ms)
+✕ should add airplane and skateboard to the end of automobiles (144 ms)
+✕ should add helicopter and bike to the front of the automobiles (138 ms)
 
 
 
