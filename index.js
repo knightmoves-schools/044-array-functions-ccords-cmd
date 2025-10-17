d3e44dbe483730016aeb4bed9e810c30a9a5a76d
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 08:44:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var automobiles = ["car", "truck", "motorcycle"];
 automobiles.push("airplane", "skateboard");
-automobiles.unshift("helicopter", "bike");
-var automobilesCopy = "automobiles";
+automobiles.unshift("bike", "helicopter");
+var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
 automobiles.pop();
 automobiles.shift();
+
 document.getElementById('result').innerHTML = automobiles;
