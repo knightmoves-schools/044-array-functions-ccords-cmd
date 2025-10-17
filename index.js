ba751792769ad15a378c6c10aed3f6d3d8443bdd
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 13:42:28 +0000

    d

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var automobiles = ["car", "truck", "motorcycle"];
 automobiles.push("airplane", "skateboard");
 automobiles.unshift("helicopter", "bike");
 var automobilesCopy = "automobiles";
-should create a variable named automobiles that contains car, truck, motorcycle
-should add airplane and skateboard to the end of automobiles
-should add helicopter and bike to the front of the automobiles
-should define a new variable called automobilesCopy and set it equal to a string template that contains the variable automobiles
-should remove the last item from the end of the the automobiles
-should remove the first item from the front of the automobiles
-should assign the innerHTML of the HTML element with the id result to automobiles
+automobiles.pop();
+automobiles.shift();
+document.getElementById('result').innerHTML = automobiles;
