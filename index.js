af83cc342ab6aca36bc51691b86f9a5ce457c4cc
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 08:50:27 -0500

    Modify automobile array and update display order

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var automobiles = ["car", "truck", "motorcycle"];
 automobiles.push("airplane", "skateboard");
-automobiles.unshift("helicopter", "bike");
-var automobilesCopy = "helicopter,bike,car,truck,motorcycle,airplane,skateboard";
+automobiles.unshift("bike,helicopter");
+var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
 automobiles.pop();
 automobiles.shift();
 
 document.getElementById('result').innerHTML = automobiles;
+
 
 
