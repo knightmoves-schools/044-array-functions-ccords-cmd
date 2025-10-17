ed89ecb705a644100a0475fa93430fc4fae79e06
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 08:53:44 -0500

    Fix unshift and pop method usage in index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var automobiles = ["car","truck","motorcycle"];
 automobiles.push("airplane", "skateboard");
-automobiles.unshift("bike,helicopter");
+automobiles.unshift("bike,helicopter",);
 var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
-automobiles.pop();
+automobiles.pop(0);
 automobiles.shift();
 
 document.getElementById('result').innerHTML = automobiles;
+
 
 
 
