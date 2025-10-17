d2f5d34a0e3c0d3ba4aa35486a3bd501948c3e25
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 09:27:43 -0500

    FUCK YOU

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 var automobiles = ["car", "truck", "motorcycle"];
-automobiles.push(,"airplane", "skateboard");
-automobiles.unshift("bike,helicopter",);
-var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
+automobiles.push("airplane", "skateboard");
+automobiles.unshift("bike", "helicopter");
+var automobilesCopy = `${automobiles}`;
 automobiles.pop();
 automobiles.shift();
-
 document.getElementById('result').innerHTML = automobiles;
-✕ should create a variable named automobiles that contains car, truck, motorcycle (257 ms)
-✕ should add airplane and skateboard to the end of automobiles (144 ms)
-✕ should add helicopter and bike to the front of the automobiles (138 ms)
-
-
-
-
-
