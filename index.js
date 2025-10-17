96da8fecd01ec89a5976c27fe3e4db88f2c7314d
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-17 08:49:13 -0500

    Reorder automobilesCopy to reflect changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var automobiles = ["car", "truck", "motorcycle"];
 automobiles.push("airplane", "skateboard");
 automobiles.unshift("helicopter", "bike");
-var automobilesCopy = "bike,helicopter,car,truck,motorcycle,airplane,skateboard";
+var automobilesCopy = "helicopter,bike,car,truck,motorcycle,airplane,skateboard";
 automobiles.pop();
 automobiles.shift();
 
 document.getElementById('result').innerHTML = automobiles;
+
 
